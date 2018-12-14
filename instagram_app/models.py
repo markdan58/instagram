72dc4881d4e27601ea86e4e5196bc80794aac8ca
markdan58 <mark.dan@strathmore.edu>
2018-12-14 11:50:06 +0300

    created image class

--- instagram_app/models.py
+++ instagram_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+
+
+
+# Create your models here.  
+class Image(models.Model):
+    name = models.CharField(max_length = 20)
+    caption = models.TextField()
+    profile = models.ForeignKey(profile)
+    comments = models.TextField()
+    like = models.ForeignKey(Category)
+    photo = models.ImageField(upload_to ="instagram_appimages/")
+
+    def __str__(self):
+        return self.name 
+
+
