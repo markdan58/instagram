d44309cf2b505a38cd887901d584beddd444c697
markdan58 <mark.dan@strathmore.edu>
2018-12-15 12:10:44 +0300

    added dispaly commit

--- instagram_app/views.py
+++ instagram_app/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
-from .models import Image
+from django.shortcuts import render,redirect
+from .models import Image,Profile,Comments
 
 
 # Create your views here.
 def displayphoto(request):
     photos = Image.objects.all()
+    commentview = Comments.objects.all()
+    print(commentview)
     return render(request,'photopage.html',{"photos":photos})
+
+
+def displayprofile(request):
+    profiles = Profile.objects.all()
+    return render(request,'profile.html',{"profiles":profiles})
+
+
+def displaycomments(request):
+    if 'Newcomments' in request.GET and request.GET["Newcomments"]:
+        comments = request.GET.get("Newcomments")
+
+        Comments.objects.create(comment = comments) 
+
+    return redirect('photopage')
+
+
+def search_results(request):
+    if 'Image' in request.GET and request.GET["Image"]:
+        image = request.GET.get("Image")
+        looked_Image = Image.search_category(image)
+        message = f"{image}"
+
+        return render(request, 'search.html',{"message":message,"images": looked_Image})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
