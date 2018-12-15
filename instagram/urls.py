967182ed00dc96fa377e386a8f77ed1bcb4f8be6
markdan58 <mark.dan@strathmore.edu>
2018-12-15 13:39:42 +0300

    created registration folder and base html

--- instagram/urls.py
+++ instagram/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('instagram_app.urls'))
+    url(r'',include('instagram_app.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
