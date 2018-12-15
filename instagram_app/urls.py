2a8bec15e7e5e14eeb702bc258f0963dc9af7b15
markdan58 <mark.dan@strathmore.edu>
2018-12-15 12:15:47 +0300

    addede the profile and commit urls

--- instagram_app/urls.py
+++ instagram_app/urls.py
@@ -6,8 +6,11 @@ from django.conf.urls.static import static
 
 
 urlpatterns=[
-    url(r'^$',views,name='photopage'),
-    url(r'^$',views,name='searchpage'),
+    url(r'^$',views.displayphoto,name='photopage'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^profile/',views.displayprofile,name='profile'),
+    url(r'^comments/',views.displaycomments,name='comments'),
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
